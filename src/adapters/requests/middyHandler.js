aaa1657d2ce5a437e2096e8befef0ee91dbb0cfd
Igor Vinicius <igorvinnicius@gmail.com>
2021-04-09 08:18:15 +0200

    implemented deploy script to use layers

---
+++ src/adapters/requests/middyHandler.js
@@ -0,0 +1,15 @@
+const middy = require('@middy/core');
+const jsonBodyParser = require('@middy/http-json-body-parser');
+const httpErrorHandler = require('@middy/http-error-handler');
+const validator = require('@middy/validator');
+
+const middyHandler = (baseHandler, inputSchema) => {    
+
+    return handler = middy(baseHandler)
+    .use(jsonBodyParser())
+    .use(validator({inputSchema}))
+    .use(httpErrorHandler());
+
+};
+
+module.exports = middyHandler;
---
+++ src/adapters/requests/requestsService.js
@@ -0,0 +1,33 @@
+module.exports = class RequestsService {
+
+    async handle(func) {
+
+        try {
+            
+            const result = await func();            
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    success: true,
+                    data: result         
+                })
+            };
+
+        } catch (error) {            
+            
+            return {
+                statusCode: error.statusCode,
+                body: JSON.stringify({
+                    success: false,
+                    statusCode: error.statusCode,
+                    error: {
+                        name: error.name,
+                        message: error.message
+                    }
+                })
+            };
+        }        
+    }
+
+}
