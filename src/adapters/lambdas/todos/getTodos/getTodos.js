ed81aa13eac10137c119238b5b9f1979663d71c3
Igor Vinicius <igorvinnicius@gmail.com>
2021-04-12 08:07:10 +0200

    implemented geTodos

---
+++ src/adapters/lambdas/todos/getTodos/getTodos.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const container = require('../../../../frameworks/dependency_injection/container');
+
+const getTodosUseCase = container.resolve('getTodosUseCase');
+const requestsService = container.resolve('requestService');
+const middyHandler = container.resolve('middyHandler');
+
+const baseHandler = async (event) => {
+
+    return await requestsService.handle(async () => {
+        
+        return await getTodosUseCase.execute();
+        
+    });
+};
+
+const handler = middyHandler(baseHandler);   
+module.exports = { handler }
