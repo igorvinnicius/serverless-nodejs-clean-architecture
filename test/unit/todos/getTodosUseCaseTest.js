a9d8b5703d36859dce4e2a253d3a51461939ae35
Igor Vinicius <igorvinnicius@gmail.com>
2021-04-12 07:06:41 +0200

    implemented geTodos unit test

---
+++ test/unit/todos/getTodosUseCaseTest.js
@@ -0,0 +1,26 @@
+const expect = require('chai');
+const sinon = require('sinon');
+
+const GetTodosUseCase = require('../../../src/application/use_cases/todos/getTodos/getTodosUseCase');
+const TodosRepository = require ('../../../src/application/contracts/repositories/todosRepository');
+const Todo = require('../../../src/entities/todo');
+
+describe('Get Todos Use Case', () => {
+
+    it('should return a list of todos', async () => {
+
+        const todosRepository = new TodosRepository();
+
+        const expectedTodos = [new Todo('Task 1'), new Todo('Task 2')]
+
+        sinon.stub(todosRepository, 'getAll').returns(expectedTodos);
+
+        const getTodosUseCase = new GetTodosUseCase(todosRepository);
+
+        const todos = await getTodosUseCase.execute();
+
+        expect.expect(todosRepository.getAll.called).to.be.true;
+        expect.expect(todos).to.be.equals(todos);    
+
+    });
+});
