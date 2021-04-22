74067b495bde24e9d7ee11197ef258faa1e93d3f
Igor Vinicius <igorvinnicius@gmail.com>
2021-04-22 07:56:12 +0200

    implemented dynamoDB common repository

--- src/frameworks/persistence/dynamoDB/todosRepository.js
+++ src/frameworks/persistence/dynamoDB/todosRepository.js
@@ -1,31 +1,12 @@
 const DynamoDBError = require('./dynamoDbError');
+const DynamoDBRepository = require('./dynamoDBRepository');
 
-module.exports = class TodosRepository {
+module.exports = class TodosRepository extends DynamoDBRepository {
 
     constructor(AWS, uuid) {
-        this.dynamoDb = new AWS.DynamoDB.DocumentClient();
-        this.uuid = uuid;
+        super(AWS, uuid);
         this.tableName = 'todos';
-    }
-
-    async add(todo) {               
-
-        todo.id = this.uuid.v1();        
-
-        const params = {
-            TableName: this.tableName,
-            Item: todo
-        };        
-
-        return await this.dynamoDb.put(params)
-        .promise()
-        .then(() => {                                  
-            return todo;
-        })
-        .catch((err) => {
-            throw new DynamoDBError(err);
-        });         
-    }
+    }    
 
     async getByName(name) {
 
@@ -45,84 +26,5 @@ module.exports = class TodosRepository {
         .catch((err) => {
             throw new DynamoDBError(err);
         });      
-    }
-
-    async getAll() {
-        var params = {
-            TableName: this.tableName              
-        };
-        
-        return  await this.dynamoDb.scan(params)
-        .promise()
-        .then((data) => {
-            return data.Count == 0 ? null : data.Items;                
-        })
-        .catch((err) => {
-            throw new DynamoDBError(err);
-        }); 
-    }
-
-    async getById(id) {
-        var params = {
-            TableName: this.tableName,
-            Key: {
-                id: id,
-            }            
-        };
-        
-        return  await this.dynamoDb.get(params)
-        .promise()
-        .then((data) => {            
-            return  data.Item == null ? null : data.Item;
-        })
-        .catch((err) => {
-            throw new DynamoDBError(err);
-        });
-    }
-
-    async update(todo) {
-
-        var params = {
-            TableName: this.tableName,
-            Key: {
-                id: todo.id,
-            },
-            ExpressionAttributeNames: {
-                '#name': 'name',
-            },
-            ExpressionAttributeValues: {
-            ':name': todo.name,  
-            },
-            UpdateExpression: 'SET #name = :name',
-            ReturnValues: 'ALL_NEW'        
-        };
-        
-        return  await this.dynamoDb.update(params)
-        .promise()
-        .then((data) => {            
-            return  data.Attributes == null ? null : data.Attributes;
-        })
-        .catch((err) => {
-            throw new DynamoDBError(err);
-        });
-    }
-
-    async delete(id) {
-
-        var params = {
-            TableName: this.tableName,
-            Key: {
-                id: id
-            }            
-        };
-        
-        return  await this.dynamoDb.delete(params)
-        .promise()
-        .then((data) => {            
-            return;
-        })
-        .catch((err) => {            
-            throw new DynamoDBError(err);
-        });
-    }
+    }      
 }
